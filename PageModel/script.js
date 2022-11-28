9c59f25241355e12f95b17f34bf199ba4a18e041
Jhonatan <jhimyend@gmail.com>
2022-11-28 14:15:35 -0400

    actualizado

--- PageModel/script.js
+++ PageModel/script.js
@@ -1,36 +1,51 @@
-//Inicializar red neuronal
-var network = new brain.NeuralNetwork();
+window.addEventListener("DOMContentLoaded",function() { 
+    var color_RGB = randomRGB();    
+    update(color_RGB);
+});
 
-//Entrenarla, darle ejemplos de cuando poner
-//texto blanco, o texto negro segun el fondo
-network.train([
-    //Fondo negro (entrada en 0s) = texto blanco (salida = 1)
-    {input: {rojo: 0, verde: 0, azul: 0}, output: {color: 1}},
-    //Fondo blanco (entrada en 1s) = texto negro (salida = 0)
-    {input: {rojo: 1, verde: 1, azul: 1}, output: {color: 0}},
-    //Fondo verde, texto negro
-    {input: {rojo: 0, verde: 1, azul: 0}, output: {color: 0}},
-    //Fondo azul, texto blanco
-    {input: {rojo: 0, verde: .43, azul: 1}, output: {color: 1}},
-    //Fondo rojo, texto blanco
-    {input: {rojo: 1, verde: 0, azul: 0}, output: {color: 1}},
-]);
 
-document.addEventListener("DOMContentLoaded",function(){
-    
+let state = 0;
+let letters = '';
+let background = new Array();
+const dislike = document.getElementById('dk');
+const like = document.getElementById('lk');
 
-},false);
 
+dislike.addEventListener('click', function(){
+    state = 1;
+    send_dataToWCF();
+});
+like.addEventListener('click', function(){
+    state = 0;
+    send_dataToWCF();
+});
+
+
+function send_dataToWCF(){
+    alert(background+'\n'+letters+'\n'+state);
+}
+
+
+function randomRGB() {
+
+  var color_indexed = new Array();
+
+  color_indexed[0] = Math.floor(Math.random() * 255);
+  color_indexed[1] = Math.floor(Math.random() * 255);
+  color_indexed[2] = Math.floor(Math.random() * 255);
+
+  return color_indexed;
+}
 
 function update(color) {
+
     //Cuando hice el video, existia color.rgb
     //Ahora existe color.channels y dentro tiene varios componentes.
     //Aqui hacemos el objeto "rgb" para que sea similar al video
-    var rgb = [color.channels.r, color.channels.g, color.channels.b];
-    //console.log(rgb);
-
+    var rgb = color;
+    background = color; 
     var div = document.getElementById("sitio");
-    div.style.background = color.toHEXString(); //Nueva forma para poner el color de fondo
+    div.style.backgroundColor = 'rgb('+rgb[0]+','+rgb[1]+','+rgb[2]+')'; //Nueva forma para poner el color de fondo
 
     //Tomar el fondo actual elegido por el usuario,
     //para usarlo de entrada para que la red nos de su
@@ -48,7 +63,26 @@ function update(color) {
     //Si resultado > .5, se considera color de texto blanco
     if (resultado.color > .5) {
         div.style.color = "white";
+        letters = "white";
     }  else {
         div.style.color = "black";
+        letters = "black";
     }
 }
+
+//Inicializar red neuronal
+var network = new brain.NeuralNetwork();
+//Entrenarla, darle ejemplos de cuando poner
+//texto blanco, o texto negro segun el fondo
+network.train([
+    //Fondo negro (entrada en 0s) = texto blanco (salida = 1)
+    {input: {rojo: 0, verde: 0, azul: 0}, output: {color: 1}},
+    //Fondo blanco (entrada en 1s) = texto negro (salida = 0)
+    {input: {rojo: 1, verde: 1, azul: 1}, output: {color: 0}},
+    //Fondo verde, texto negro
+    {input: {rojo: 0, verde: 1, azul: 0}, output: {color: 0}},
+    //Fondo azul, texto blanco
+    {input: {rojo: 0, verde: .43, azul: 1}, output: {color: 1}},
+    //Fondo rojo, texto blanco
+    {input: {rojo: 1, verde: 0, azul: 0}, output: {color: 1}},
+]);
